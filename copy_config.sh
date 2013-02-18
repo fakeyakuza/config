b87c33b4d81cd4c6d3e57cf433c96bfdd4de35ce
kisho <kisho@localhost.localdomain>
2013-02-18 15:17:55 +0900

    modified:   copy_config.sh

--- copy_config.sh
+++ copy_config.sh
@@ -6,8 +6,8 @@ cp ~/.bashrc .
 cp ~/.screenrc .
 cp ~/.tmux.conf .
 
-cp . ~/.inputrc 
-cp . ~/.zshrc 
-cp . ~/.bashrc 
-cp . ~/.screenrc 
-cp . ~/.tmux.conf 
+cp .inputrc ~/.inputrc 
+cp .zshrc ~/.zshrc 
+cp .bashrc ~/.bashrc 
+cp .screenrc ~/.screenrc 
+cp .tmux.conf ~/.tmux.conf 
